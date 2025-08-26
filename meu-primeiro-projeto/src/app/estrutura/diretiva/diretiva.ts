dd4f3619268ff0b2358ed9ef872b24caaccc0f8c
Otávio Volpe Leal <126003350+OtavioVolpe@users.noreply.github.com>
2025-08-26 00:38:52 +0000

    Exercicio de Nome

---
+++ meu-primeiro-projeto/src/app/estrutura/diretiva/diretiva.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva',
+  standalone: false,
+  templateUrl: './diretiva.html',
+  styleUrl: './diretiva.css'
+})
+export class Diretiva {
+  conteudo = false;
+
+  mostrarOcultar() {
+    this.conteudo = !this.conteudo;
+  }
+
+  listaItens = [
+    {id: '001', nome: 'volpe', ativo: true},
+    {id: '002', nome: 'biel', ativo: false},
+    {id: '003', nome: 'luiz', ativo: true},
+    {id: '004', nome: 'elo', ativo: false}
+  ];
+
+}
--- meu-primeiro-projeto/src/app/livros/cadastrar/cadastrar.ts
+++ meu-primeiro-projeto/src/app/livros/cadastrar/cadastrar.ts
@@ -1,3 +1,4 @@
+import { TestRequest } from '@angular/common/http/testing';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,7 +12,25 @@ export class Cadastrar {
 
     caminhoImagem ='/workspaces/projeto-integrador-iv/meu-primeiro-projeto/public/favicon.ico';
 
-    dizerOi(){
-      this.nome = 'Alunod do 4B';
+    mostrarMensagem = false;
+
+    listaItens = ['Maça', 'Banana', 'Laranja'];
+
+    toggleMensagem() {
+      this.mostrarMensagem = !this.mostrarMensagem;
+      this.isDestaque = false;
+      this.hasError = true;
     }
+
+    dizerOi(){
+      this.nome = 'Alunos do 4B';
+    }
+
+    isDestaque = true;
+    hasError = false;
+
+    corFundo = 'yellow';
+    tamanhoFonte = '20px';
+
+
 }
